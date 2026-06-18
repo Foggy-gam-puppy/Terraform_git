c1bdcbc20dcade830ddd925665adf4a308242666
Foggy-gam-puppy <119307603+Foggy-gam-puppy@users.noreply.github.com>
2026-06-19 00:23:49 +0300

    Update loadbalancer.tf

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -1,14 +1,12 @@
-# 1. Сам Балансировщик Нагрузки
+# 1. Балансировщик нагрузки
 resource "vkcs_lb_loadbalancer" "lb" {
   name          = "${var.project_name}-loadbalancer"
   description   = "Loadbalancer for web servers"
   vip_subnet_id = vkcs_networking_subnet.public_subnet.id
 
-  # ИСПРАВЛЕНО: Убран устаревший аргумент security_group_ids
-  # ИСПРАВЛЕНО: Зависимости depends_on Terraform строит сам, они здесь лишние
 }
 
-# 2. Слушатель (Слушает входящий порт 80)
+# 2. Слушатель 
 resource "vkcs_lb_listener" "lb_http" {
   name            = "${var.project_name}-listener-http"
   loadbalancer_id = vkcs_lb_loadbalancer.lb.id
@@ -21,10 +19,10 @@ resource "vkcs_lb_pool" "http" {
   name        = "${var.project_name}-pool-http"
   listener_id = vkcs_lb_listener.lb_http.id
   protocol    = "HTTP"
-  lb_method   = "ROUND_ROBIN" # Алгоритм распределения трафика по очереди
+  lb_method   = "ROUND_ROBIN" 
 }
 
-# 4. Проверка работоспособности (Health Check)
+# 4. Проверка работоспособности
 resource "vkcs_lb_monitor" "worker_ping_life_checker" {
   name        = "${var.project_name}-health-monitor"
   pool_id     = vkcs_lb_pool.http.id
@@ -37,27 +35,20 @@ resource "vkcs_lb_monitor" "worker_ping_life_checker" {
 
 # 5. Привязка веб-серверов к Балансировщику
 resource "vkcs_lb_member" "web_servers_members" {
-  # Перебираем наши веб-серверы
   for_each      = vkcs_compute_instance.web_server 
   pool_id       = vkcs_lb_pool.http.id
   protocol_port = 80                               
   subnet_id     = vkcs_networking_subnet.private_subnet.id
 
-  # ИСПРАВЛЕНО: Надежный способ забрать IP-адрес через готовые порты
   address       = vkcs_networking_port.web_server_port[each.key].all_fixed_ips[0]
 }
 
 
-
-
-
-
 resource "local_file" "k8s_configmap" {
-  # Указываем путь к шаблону на уровень выше (в корень проекта)
+  
   content = templatefile("${path.module}/../templates/configmap.yaml.tpl", {
     lb_ip = vkcs_lb_loadbalancer.lb.vip_address
   })
   
-  # Сохраняем сгенерированный файл в папку kubernetes на уровень выше
   filename = "${path.module}/../kubernetes/configmap.yaml"
 }
